c458cf56c579fbb09de51abb8427b77c246969cb
Sebastian <sebastian@eiweleit.de>
2022-03-09 12:09:22 +0100

    Tune the varDebug a little
    Accordion and optical improvements for varDebug

--- src/functions.php
+++ src/functions.php
@@ -67,7 +67,7 @@ if (!function_exists('slugify')) {
 }
 
 
-if (!function_exists('vdarDebug')) {
+if (!function_exists('varDebug')) {
     /**
      * Dump all passed variables and exit the script
      * @param ...$mixed
@@ -76,28 +76,33 @@ if (!function_exists('vdarDebug')) {
     {
         http_response_code(503);
         ob_end_clean();
-        echo '<html><head><title>varDebug</title><meta charset="utf-8"><style>body{ background-color: #2D4263; color: #EEEEEE; padding: 1em; font-family: "Ubuntu Light",serif; font-weight: lighter } a { color: #C84B31;} a:hover {color:#A13333} pre { overflow: auto; } h2 span { font-size: .7em; float: right; font-weight: lighter; background-color: #C84B31; padding: .2em; } h2 span:hover { font-size: .7em; float: right; font-weight: lighter; background-color: #A13333; cursor: pointer; } code { display: block; padding: 1em; border-radius: .4em; background-color: #191919; font-family: "Ubuntu Mono",monospace; font-size: 1.1em; line-height: 1.5em; }</style></head><body>';
-        echo '<h1>Debugging ' . count($mixed) . ' Items</h1>';
+        echo '<html><head><title>varDebug</title><meta charset="utf-8"><style>body{ background-color: #2D4263; color: #EEEEEE; padding: 1em; font-family: "Ubuntu Light",serif; font-weight: lighter; max-width: 99vw; } a { color: #C84B31;} a:hover {color:#A13333} pre { overflow: auto; } label span { position: absolute; right: 15vw; } pre { height: 0; margin:0; } label span, .copy { font-size: .7em; float: right; font-weight: lighter; background-color: #C84B31; padding: .2em; } label span:hover, .copy:hover { font-size: .7em; float: right; font-weight: lighter; background-color: #A13333; cursor: pointer; } code { display: block; padding: 1em; border-radius: .4em; background-color: #191919; font-family: "Ubuntu Mono",monospace; font-size: 1.1em; line-height: 1.5em; } footer { padding: 1em; text-align: right; } .accordion { overflow: hidden; box-shadow: 0 4px 4px -2px rgba(0, 0, 0, 0.5); border-radius: 8px; } .accordion label { border-radius-topleft: 8px; border-radius-topright: 8px; display: flex; justify-content: space-between; padding: 1em; background: #2c3e50; font-weight: bold; cursor: pointer; } .accordion label:hover { background: #1a252f; } .accordion label::after { content: "❯"; width: 1em; height: 1em; text-align: center; transition: all 0.35s; } .accordion input { display: none; } .accordion pre code { border-radius: 0; } .accordion input:checked + label { background: #1a252f; } .accordion input:checked + label::after { transform: rotate(90deg); } .accordion input:checked ~ pre { height: auto; } </style></head><body>';
+
+        echo '<div class="accordion">' . PHP_EOL;
         $x = 0;
         foreach ($mixed as $item) {
             $x++;
-            echo '<h2>#'.$x.' <span class="google" data-debug="debug_'.$x.'">Google</span></h2><pre><code id="debug_'.$x.'">';
+            $xx = time() . $x;
+            echo '<div><input type="checkbox" id="_debug_'.$xx.'" checked /><label for="_debug_'.$xx.'">#'.$x.' <span class="google" data-debug="debug_'.$xx.'">Google</span></label> <pre><code id="debug_'.$xx.'">';
             var_dump($item);
-            echo '</code></pre>';
+            echo '</code></pre></div>';
         }
 
-        echo'<h2>Backtrace</h2><pre><code>';
+        echo'<div id="backtrace"><input type="checkbox" id="_backtrace"><label for="_backtrace">Backtrace</label> <pre><code>';
         debug_print_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
-        echo '</code></pre>';
+        echo '</code></pre></div> ';
 
-        echo '<h2>Server-Data</h2><pre><code>';
-        foreach ($_SERVER as $name => $value) {
-            echo '$_SERVER["' . $name . '"]: ' . $value ."\n";
+        $enviremental = ['SERVER' => $_SERVER, 'POST' => $_POST,'GET' => $_GET, 'REQUEST' => $_REQUEST ];
+
+        foreach ($enviremental  as $env => $values) {
+            echo '<div id="' . $env . '"><input type="checkbox" id="_'.$env.'"><label for="_'.$env.'">$_'.$env.'</label> <pre><code>';
+            var_dump($enviremental[$env]);
+            echo '</code></pre></div>' . PHP_EOL;
         }
-        echo '</code></pre> <script> document.querySelectorAll("span.google").forEach(function (element, index, array){ element.addEventListener("click", function() { window.open("https://www.google.com/search?q=" + encodeURI("+php " + document.querySelector("#" + element.getAttribute("data-debug")).innerHTML), "_blank"); }) }); </script> <footer>Problems of suggestions? Submit it on <a href="https://github.com/basteyy/various-php-snippets">github/basteyy/various-php-snippets</a></footer></body></html>';
 
+        echo '</div><script> document.querySelectorAll("span.google").forEach(function (element, index, array){ element.addEventListener("click", function() { window.open("https://www.google.com/search?q=" + encodeURI("+php " + document.querySelector("#" + element.getAttribute("data-debug")).innerHTML), "_blank"); }) }); </script> <footer>Problems of suggestions? Submit it on <a href="https://github.com/basteyy/various-php-snippets">github/basteyy/various-php-snippets</a></footer></body></html>';
 
-        die();
+        exit();
     }
 }
 
